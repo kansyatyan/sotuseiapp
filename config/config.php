141449565570f8ba7ebff4e6437312c34397b5c4
49 <21cm0149@jec.ac.jp>
2023-02-22 14:48:54 +0900

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+define('DSN','mysql:host=mysql207.phy.lolipop.lan;dbname=LAA1479619-sotuseiapp');
+define('DB_USER','LAA1479619');
+define('DB_PASSWORD','gsirngringgr');
+error_reporting(E_ALL & ~E_NOTICE);
